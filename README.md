4a6a96a7bf14dcee72ac85aca334ae2eb33681db
Andrew <andrew@andrewperlas.com>
2024-06-11 10:22:25 -1000

    create quiz-game python program

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # python-quiz-game
+
+## What does this program do?
+- Ask user if they want to play
+    - End program if they do not want to play
+- Ask user a variety of questions
+- If they answer correctly, +1 to their score
+- At the end of the program, print the number of corrects answers out of how many total questions
---
+++ quiz-game.py
@@ -0,0 +1,42 @@
+print("Welcome to Andrew's computer quiz!")
+
+# Ask user if they want to play
+playing = input("Do you want to play? ")
+
+# If user's response is anything other than "yes", then end the program
+# .lower() to account for any uppercase characters like "YES"
+if playing.lower() != "yes":
+    print("Okay, bye!")
+    quit()
+
+print("Let's play :)")
+score = 0
+
+# if/else statements to determine if user's answers are correct or not
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
